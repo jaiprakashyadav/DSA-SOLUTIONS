51297f267e913a21d5c5a82badbed9cde0cfce53
Jai Prakash Yadav <60145457+jaiprakashyadav@users.noreply.github.com>
2021-08-18 19:19:19 +0530

    Create Meeting rooms.cpp

---
+++ Greedy/Meeting rooms.cpp
@@ -0,0 +1,28 @@
+int Solution::solve(vector<vector<int> > &A) 
+{
+    int n=A.size();
+    vector<int>start(n),finish(n);
+    for(int i=0;i<n;i++)
+    {
+        start[i]=A[i][0];
+        finish[i]=A[i][1];
+    }
+    sort(start.begin(),start.end());
+    sort(finish.begin(),finish.end());
+    int room=0,i=0,j=0,ans=0;
+    while(i<n && j<n)
+    {
+        if(start[i]<finish[j])
+        {
+            room++;
+            i++;
+        }
+        else
+        {
+            room--;
+            j++;
+        }
+        ans=max(ans,room);
+    }
+    return ans;
+}
